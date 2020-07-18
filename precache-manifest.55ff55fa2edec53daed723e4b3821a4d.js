49e4e029e6a565a386675a5f145f953ee29f2025
Mariya <mariya.podosinova@gmail.com>
2020-07-18 16:46:46 +0100

    Updates

---
+++ precache-manifest.55ff55fa2edec53daed723e4b3821a4d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74577bf491082bfc868b036a9f29aa25",
+    "url": "/delivery/index.html"
+  },
+  {
+    "revision": "40c5947840c504c65adf",
+    "url": "/delivery/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6888600ff3053d4b3e44",
+    "url": "/delivery/static/css/main.762bcaa6.chunk.css"
+  },
+  {
+    "revision": "40c5947840c504c65adf",
+    "url": "/delivery/static/js/2.42fbac87.chunk.js"
+  },
+  {
+    "revision": "031b62b957b4fb917cd0ec81e2c8bd7b",
+    "url": "/delivery/static/js/2.42fbac87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6888600ff3053d4b3e44",
+    "url": "/delivery/static/js/main.bf1c019b.chunk.js"
+  },
+  {
+    "revision": "febbdb5fd88a4563b76d",
+    "url": "/delivery/static/js/runtime-main.785fe610.js"
+  },
+  {
+    "revision": "c49e25750e4c721510e8818b15de26f4",
+    "url": "/delivery/static/media/bigBalloon.c49e2575.jpg"
+  }
+]);
